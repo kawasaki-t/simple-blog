2d61fd8b668a1299f4da85246c66ce9f7aa82bbe
kawasaki-t <36919582+kawasaki-t@users.noreply.github.com>
2018-06-06 20:23:45 +0900

    Update App.js

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from 'react-apollo';
 import ArticleList from './ArticleList';
 
 const link = new HttpLink({
-  uri: 'https://api.graphcms.com/simple/v1/cj98b2tds1fha0115h7csyj5d',
+  uri: 'https://api.graphcms.com/simple/v1/cji2zthzp2t1y0119ull141aj',
 });
 const cache = new InMemoryCache();
 const client = new ApolloClient({ link, cache });
